c2f0c0a743dd1e0b8c4b36265dee082796fdb64d
QPThree <youngqp3@gmail.com>
2024-11-28 20:17:36 -0800

    Adds select options
    Adds select options

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useAuth0 } from '@auth0/auth0-react';
-import { AppShell, Badge, Button, Burger, Space, Text } from '@mantine/core';
+import { AppShell, Badge, Button, Burger, Group, Radio, Select, Space, Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import HomePageUnAuth from './Pages/HomePage/HomePageUnAuth'
 
@@ -11,6 +11,9 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [opened, { toggle }] = useDisclosure();
+  const [timeDomain, setTimeDomain] = useState('60')
+  const [selectedValues, setSelectedValues] = useState({
+  });
 
   const {
     logout,
@@ -19,23 +22,52 @@ function App() {
     isLoading,
   } = useAuth0();
 
-  const fakeData = {
-    timeFrame: '30',
-    movements: ['deadlift', 'pushups', 'double-unders'],
-    focus: 'strength'
 
-  };
   const colors = ['red', 'violet', 'indigo', 'yellow', 'teal'];
   useEffect(() => {
     // Ensure the user is authenticated before making the request
     if (isAuthenticated && user.email) {
       setLoading(true)
-      axios.get('https://uz45ft8e8h.execute-api.us-west-2.amazonaws.com',  {
-        params: fakeData,
+      axios.get('https://uz45ft8e8h.execute-api.us-west-2.amazonaws.com', {
+        params: {},
         headers: {
           'user': JSON.stringify(user),  // Add your custom header here
         }
       })
+        .then(response => {
+          console.log(response)
+          setData(response.data);  // Store the response data
+          setLoading(false);
+        })
+        .catch(error => {
+          setError(error.message);  // Handle any errors
+          setLoading(false);
+        });
+    }
+  }, [isAuthenticated, user]); // Add dependencies for re-running effect when they change
+
+  // Handle the change for each Select
+  const handleSelectChange = (fieldName) => (value) => {
+    setSelectedValues((prevState) => ({
+      ...prevState,
+      [fieldName]: value,
+    }));
+
+  };
+
+  // Send the selected values to the backend
+  const handleSubmit = () => {
+    // Send selectedValues as a list or in the format expected by the backend
+
+    setLoading(true)
+    const valuesToSend = selectedValues
+    valuesToSend['timeDomain'] = timeDomain
+    axios.get('https://uz45ft8e8h.execute-api.us-west-2.amazonaws.com', {
+      params: valuesToSend,
+      headers: {
+        'user': JSON.stringify(user),  // Add your custom header here
+      }
+    })
       .then(response => {
         console.log(response)
         setData(response.data);  // Store the response data
@@ -45,8 +77,7 @@ function App() {
         setError(error.message);  // Handle any errors
         setLoading(false);
       });
-    }
-  }, [isAuthenticated, user]); // Add dependencies for re-running effect when they change
+  };
 
   if (isLoading) {
     return <div>Loading....</div>;
@@ -60,45 +91,96 @@ function App() {
     !isAuthenticated ? (
       <HomePageUnAuth />
     ) : (
-    <div>
-         <AppShell
-      header={{ height: 60 }}
-      navbar={{
-        width: 300,
-        breakpoint: 'sm',
-        collapsed: { mobile: !opened },
-      }}
-      padding="md"
-    >
-      <AppShell.Header>
-      <Burger
-          lineSize={2} 
-          size="xl"
-          opened={opened}
-          onClick={toggle}
-          hiddenFrom="sm"
-        />
-      </AppShell.Header>
-      <AppShell.Navbar p="md">
-        <Text size="xl" c="blue" fw={700}>Welcome, {user.name.split(" ")[0]} </Text>
-        <div>Past Workouts</div>
-      <Button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</Button>
-       </AppShell.Navbar>
-      <AppShell.Main>
-        {loading? <div>Loading...</div>: 
-      <>
-      <h1>Today's WOD</h1>
-      {data?.themes.map((theme, index) => {
-        const badgeColor = colors[index % colors.length]; // Cycle through colors
-        return <Badge color={badgeColor} key={index}>{theme}</Badge>;
-      })}
-      <Space h="md" />
-      <div dangerouslySetInnerHTML={{ __html: data?.workout }} />
-      </>
-        }
-      </AppShell.Main>
-      </AppShell>
-    </div>
+      <div>
+        <AppShell
+          header={{ height: 60 }}
+          navbar={{
+            width: 300,
+            breakpoint: 'sm',
+            collapsed: { mobile: !opened },
+          }}
+          padding="md"
+        >
+          <AppShell.Header>
+            <Burger
+              lineSize={2}
+              size="xl"
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+            />
+          </AppShell.Header>
+          <AppShell.Navbar p="md">
+            <Text size="xl" c="blue" fw={700}>Welcome, {user.name.split(" ")[0]} </Text>
+            <div>Past Workouts</div>
+            <Button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</Button>
+          </AppShell.Navbar>
+          <AppShell.Main>
+            {loading ? <div>Loading...</div> :
+              <>
+                <h1>Today's WOD</h1>
+                {data?.themes?.map((theme, index) => {
+                  const badgeColor = colors[index % colors.length]; // Cycle through colors
+                  return <Badge color={badgeColor} key={index}>{theme}</Badge>;
+                })}
+                <Space h="md" />
+                <div dangerouslySetInnerHTML={{ __html: data?.workout }} />
+
+                {!data?.workout &&
+                  <>
+                    <h2>Create New WOD</h2>
+                    <Radio.Group
+                      name="favoriteFramework"
+                      label="Select your favorite framework/library"
+                      description="This is anonymous"
+                      withAsterisk
+                    >
+                      <Group mt="xs" value={timeDomain ?? '60'} onChange={event => setTimeDomain(event.target.value)}>
+                        <Radio value="10" label="10" />
+                        <Radio value="30" label="30" />
+                        <Radio value="60" label="60" />
+                        <Radio value="90" label="90" />
+                      </Group>
+                    </Radio.Group>
+
+                    <Text>Optional select any of the options below</Text>
+
+                    <Select
+                      label="Barbell movements"
+                      placeholder="Pick value"
+                      data={['Deadlifts', 'Back Squats', 'Cleans', 'Snatches', 'deadlifts']}
+                      value={selectedValues.favoriteLibrary2}
+                      onChange={handleSelectChange('Barbell')}
+                    />
+                    <Select
+                      label="Skills"
+                      placeholder="Pick value"
+                      data={['double unders', 'handstand pushups', 'muscle ups']}
+                      value={selectedValues.favoriteLibrary3}
+                      onChange={handleSelectChange('Skills')}
+                    />
+
+                    <Select
+                      label="Endurance"
+                      placeholder="Pick value"
+                      data={['rowing', 'running', 'erg biking']}
+                      value={selectedValues.favoriteLibrary4}
+                      onChange={handleSelectChange('Endurance')}
+                    />
+
+
+
+                    <Button variant="filled" color="green" onClick={() => handleSubmit()}>Button</Button>
+                  </>
+                }
+
+
+
+              </>
+            }
+          </AppShell.Main>
+        </AppShell>
+      </div >
     ));
 }
 
