a93187119a25688a98c5b7e4df435ae282524af2
QPThree <youngqp3@gmail.com>
2024-11-29 18:29:48 -0800

    Stacks badges vertically
    Stacks badges vertically

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,11 @@
-import { AppShell, Card, Container, Image, Text, Badge, Button, Group, Space } from '@mantine/core';
+import { AppShell, Card, Container, Image, Text, Badge, Button, Group, Stack } from '@mantine/core';
 
 export const renderThemeBadges = (themes) => {
     const themeColorMap = {
         'strength': 'red',
         'endurance': 'green',
         'cardio': 'green',
+        'conditioning': 'green',
         'bodyweight': 'yellow',
         'skill': 'yellow',
         'gymnastics': 'blue',
@@ -12,9 +13,21 @@ export const renderThemeBadges = (themes) => {
         'full-body': 'red'
     };
     const defaultColor = 'grey'
-    return themes?.map((theme, index) => {
-        // Use the predefined color for a theme if it exists, otherwise use the default color
-        const badgeColor = themeColorMap[theme] || defaultColor;
-        return <Badge color={badgeColor} key={index}>{theme}</Badge>;
-    });
+    return (
+        <Stack
+            h={300}
+            bg="var(--mantine-color-body)"
+            align="stretch"
+            justify="center"
+            gap="md"
+        >
+            {themes?.map((theme, index) => {
+                // Use the predefined color for a theme if it exists, otherwise use the default color
+                const badgeColor = themeColorMap[theme] || defaultColor;
+                return <Badge color={badgeColor} key={index}>{theme}</Badge>
+
+            })}
+        </Stack>
+    )
+
 };
